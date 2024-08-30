01b9c16091480b168b30f7a877318ab478f03d72
jbreno <joao.breno.ro17@gmail.com>
2024-08-29 23:10:05 -0300

    wip: ainda lutando com o java

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 https://jasonrbriggs.github.io/stomp.py/quickstart.html#installation
 
 https://drive.google.com/file/d/1m-VIYiPyDnNqc8_ILG4XOEPzFBBS7US4/view
+
+https://www.jetbrains.com/help/idea/compiling-applications.html#add_file_to_jar
+
+instalar intelliJ
--- examples/Producer.java
+++ examples/Producer.java
@@ -10,21 +10,25 @@ public class Producer
 
     public static void main(String[] args) throws JMSException {
 
-        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
-        Connection connection = connectionFactory.createConnection();
-        connection.start();
+        try {
+            ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
+            Connection connection = connectionFactory.createConnection();
+            connection.start();
 
-        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
-        Destination destination = session.createQueue(queueName);
+            Destination destination = session.createQueue(queueName);
 
-        MessageProducer producer = session.createProducer(destination);
-        TextMessage message = session.createTextMessage("Mensagem do Produtor.");
+            MessageProducer producer = session.createProducer(destination);
+            TextMessage message = session.createTextMessage("Mensagem do Produtor.");
 
-        producer.send(message);
+            producer.send(message);
 
-        producer.close();
-        session.close();
-        connection.close();
+            producer.close();
+            session.close();
+            connection.close();
+        } catch (Exception e) {
+            System.out.println(e);;
+        }
     }
 }
